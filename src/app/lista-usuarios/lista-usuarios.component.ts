5d05a6d1eae6d4b222a822822affcbff2e821813
Sebastian Ledesma <sevaledesma@gmail.com>
2022-10-04 15:25:40 -0300

    Servicios Usuario

--- src/app/lista-usuarios/lista-usuarios.component.ts
+++ src/app/lista-usuarios/lista-usuarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../usuario';
+import { UsuarioService } from '../usuario.service';
 
 @Component({
   selector: 'app-lista-usuarios',
@@ -10,31 +11,15 @@ export class ListaUsuariosComponent implements OnInit {
 
   usuarios:Usuario[];
 
-  constructor() { }
+  constructor(private usuarioServicio:UsuarioService) { }
 
   ngOnInit(): void {
-    this.usuarios = [{
-      "id":1,
-      "nombre":"Sebastian",
-      "apellidoP":"Ledesma",
-      "apellidoM":"Genga",
-      "sanctions":"0",
-      "sanc_money": "0",
-      "tel": "3516485145",
-      "domicilio": "Agustin"
-    },
-    {
-      "id":1,
-      "nombre":"Diego",
-      "apellidoP":"Ledesma",
-      "apellidoM":"Genga",
-      "sanctions":"0",
-      "sanc_money": "0",
-      "tel": "3516485145",
-      "domicilio": "Agustin"
-
-    }
-  ];
+    this.obtenerUsuario();
   }
+private obtenerUsuario(){
+  this.usuarioServicio.obtenerListaUsuarios().subscribe(dato=>{
+    this.usuarios = dato
+  });
+}
 
 }
--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from './usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  //Llamada a la clase Usuario
+
+  private baseURL = "http://localhost:8080/api/v1/usuarios";
+
+  constructor(private httpCient : HttpClient ) { }
+
+  //Metodo para obtener usuarios.
+
+    obtenerListaUsuarios():Observable<Usuario[]>{
+
+      return this.httpCient.get<Usuario[]>('${this.baseURL}');
+    }
+
 }
