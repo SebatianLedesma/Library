b30b723a1a9ffb6fc044ad1632d7d3f14921277e
Sebastian Ledesma <sevaledesma@gmail.com>
2022-10-04 17:22:20 -0300

    Agregado de NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 import {HttpClientModule} from '@angular/common/http';
+import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaUsuariosComponent
+    ListaUsuariosComponent,
+    RegistrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
